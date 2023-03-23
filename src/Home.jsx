e57ad1a97f7f8c95b7e803edde6d404908fd35ff
Thomas <tmartin861@gmail.com>
2023-03-23 10:14:24 -0400

    Commited latest changes, should be fully intregrated now

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,67 +6,71 @@ import "./ListPlanets.jsx";
 // import ListPlanets from "./ListPlanets.jsx";
 
 export default function Home() {
-  const [Planets, setPlanets] = useState([
-    {
-      name: "Earth",
-      funfact: "Did you know earth is 70% water?",
-      img: "url",
-      id: "0",
-    },
-    {
-      name: "Mars",
-      funfact: "This is mars, woooww",
-      img: "url",
-      id: "1",
-    },
-    {
-      name: "Jupiter",
-      funfact: "This is mars, woooww",
-      img: "url",
-      id: "2",
-    },
-    {
-      name: "Saturn",
-      funfact: "This is saturn, woooww",
-      img: "url",
-      id: "3",
-    },
-  ]);
+    const [Planets, setPlanets] = useState([
+        {
+            name: "Earth",
+            funfact: "Did you know earth is 70% water?",
+            img: "url",
+            id: "0",
+        },
+        {
+            name: "Mars",
+            funfact: "This is mars, woooww",
+            img: "url",
+            id: "1",
+        },
+        {
+            name: "Jupiter",
+            funfact: "This is mars, woooww",
+            img: "url",
+            id: "2",
+        },
+        {
+            name: "Saturn",
+            funfact: "This is saturn, woooww",
+            img: "url",
+            id: "3",
+        },
+    ]);
 
-  const [ActivePlanet, setActivePlanet] = useState("");
-  const [PlanetFacts, setPlanetFacts] = useState("");
+    const [ActivePlanet, setActivePlanet] = useState("");
+    const [PlanetFacts, setPlanetFacts] = useState("");
 
 
-  if (ActivePlanet == "" && PlanetFacts == "")
-    return (
-      <div className="Home">
-        <h1> <FaStar /> ADVENTURES WITH SPACE INVADERS <FaStar />  </h1>
-        <h2>Which planet do you want to explore?</h2>
-        
-        <button onClick={() => setActivePlanet("Earth")} >Earth <FaSpaceShuttle />  </button>
-        <button onClick={() => setActivePlanet("Mars")}>Mars <FaSpaceShuttle /></button>
-        <button onClick={() => setActivePlanet("Jupiter")}>Jupiter <FaSpaceShuttle />  </button>
 
-        <div>
-          <ul>
-            {/* {Planets.map((planet) => (
+    if (ActivePlanet == "")
+        return (
+            <div className="Home">
+                <h1> <FaStar /> ADVENTURES WITH SPACE INVADERS <FaStar />  </h1>
+                <h2>Which planet do you want to explore?</h2>
+
+                <button onClick={() => setActivePlanet("Earth")} >Earth <FaSpaceShuttle />  </button>
+                <button onClick={() => setActivePlanet("Mars")}>Mars <FaSpaceShuttle /></button>
+                <button onClick={() => setActivePlanet("Jupiter")}>Jupiter <FaSpaceShuttle />  </button>
+
+                <img src="https://img.freepik.com/free-vector/paper-style-galaxy-background_23-2148985024.jpg" alt="" />
+
+                <div>
+                    <ul>
+                        {/* {Planets.map((planet) => (
               <ListPlanets singlePlanet={planet} /> */}
-            {/* ))} */}
-          </ul>
+                        {/* ))} */}
+                    </ul>
 
-          <ul></ul>
-        </div>
-      </div>
-    );
-  else
-    return (
-      <div>
-        <h1>{ActivePlanet}</h1>
-        <p>{PlanetFacts}</p>
+                    <ul></ul>
+                </div>
+            </div>
+        );
+    else
+        return (
+            <div>
+                <h1>{ActivePlanet}</h1>
+                <p>{PlanetFacts}</p>
+                <img src="" alt="" />
 
-        <button onClick={() => setPlanetFacts("")}>View Photo</button>
-        <button onClick={() => setPlanetFacts("")}>See more</button>
-        <button onClick={() => setActivePlanet("")}>Go back</button>
-      </div>
-    );
+                <button onClick={() => setPlanetFacts("Here is the photo of the planet")}>View Photo</button>
+                <button onClick={() => setPlanetFacts("Here is some imformation")}>See more</button>
+                <button onClick={() => setActivePlanet("")}>Go back</button>
+            </div>
+        );
 }
